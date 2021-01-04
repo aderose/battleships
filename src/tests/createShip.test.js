7df0e132f33c2edc3c3a11474996adaf57485876
aderose <sandro.derose28@yahoo.com>
2021-01-04 15:16:09 +0000

    Adjust typo in test message

--- src/tests/createShip.test.js
+++ src/tests/createShip.test.js
@@ -1,6 +1,6 @@
 import createShip from '../factories/createShip';
 
-test('Throw error if argument is not a number', () => {
+test('Throw error if length is not a number', () => {
   expect(() => createShip()).toThrow('Length must be a number');
   expect(() => createShip(undefined)).toThrow('Length must be a number');
   expect(() => createShip(null)).toThrow('Length must be a number');
