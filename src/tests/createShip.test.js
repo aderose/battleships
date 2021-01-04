1d3d2b1227c78fe2688052a87ecbf7f8ddb8ef59
aderose <sandro.derose28@yahoo.com>
2021-01-04 15:11:35 +0000

    Adjust error message

--- src/tests/createShip.test.js
+++ src/tests/createShip.test.js
@@ -1,12 +1,12 @@
 import createShip from '../factories/createShip';
 
 test('Throw error if argument is not a number', () => {
-  expect(() => createShip()).toThrow('Argument must be a number');
-  expect(() => createShip(undefined)).toThrow('Argument must be a number');
-  expect(() => createShip(null)).toThrow('Argument must be a number');
-  expect(() => createShip(true)).toThrow('Argument must be a number');
-  expect(() => createShip([])).toThrow('Argument must be a number');
-  expect(() => createShip({})).toThrow('Argument must be a number');
-  expect(() => createShip('')).toThrow('Argument must be a number');
-  expect(() => createShip(() => {})).toThrow('Argument must be a number');
+  expect(() => createShip()).toThrow('Length must be a number');
+  expect(() => createShip(undefined)).toThrow('Length must be a number');
+  expect(() => createShip(null)).toThrow('Length must be a number');
+  expect(() => createShip(true)).toThrow('Length must be a number');
+  expect(() => createShip([])).toThrow('Length must be a number');
+  expect(() => createShip({})).toThrow('Length must be a number');
+  expect(() => createShip('')).toThrow('Length must be a number');
+  expect(() => createShip(() => {})).toThrow('Length must be a number');
 });
