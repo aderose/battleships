8bca72a34b3fe0d5670e83a10925ccda5d9c481c
aderose <sandro.derose28@yahoo.com>
2021-01-04 15:36:50 +0000

    Return an object

--- src/factories/createShip.js
+++ src/factories/createShip.js
@@ -2,6 +2,7 @@ const createShip = (length) => {
   if (typeof length !== 'number') throw new Error('Length must be a number');
   if (![2, 3, 4, 5].includes(length))
     throw new Error('Length can only be 2, 3, 4 or 5');
+  return {};
 };
 
 export default createShip;
