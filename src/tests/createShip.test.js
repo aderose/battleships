134ee70e7cf287ce291509317f269f1f59421c01
aderose <sandro.derose28@yahoo.com>
2021-01-04 15:23:27 +0000

    Test input numerical value

--- src/tests/createShip.test.js
+++ src/tests/createShip.test.js
@@ -10,3 +10,9 @@ test('Throw error if length is not a number', () => {
   expect(() => createShip('')).toThrow('Length must be a number');
   expect(() => createShip(() => {})).toThrow('Length must be a number');
 });
+
+test('Throw error if length is not in the array [5, 4, 3, 3, 2]', () => {
+  expect(() => createShip(1)).toThrow('Length can only be 2, 3, 4 or 5');
+  expect(() => createShip(-100)).toThrow('Length can only be 2, 3, 4 or 5');
+  expect(() => createShip(0.980394)).toThrow('Length can only be 2, 3, 4 or 5');
+});
