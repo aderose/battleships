30f8540494209c25a77f3f195f2235c1193ec0a0
aderose <sandro.derose28@yahoo.com>
2021-01-04 15:55:13 +0000

    Return an object containing three properties

--- src/factories/createShip.js
+++ src/factories/createShip.js
@@ -2,7 +2,11 @@ const createShip = (length) => {
   if (typeof length !== 'number') throw new Error('Length must be a number');
   if (![2, 3, 4, 5].includes(length))
     throw new Error('Length can only be 2, 3, 4 or 5');
-  return {};
+  return {
+    prop1: '',
+    prop2: '',
+    prop3: '',
+  };
 };
 
 export default createShip;
