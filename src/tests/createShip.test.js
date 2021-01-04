6089a391c527c40399cf8a8fc1b915e1882a924f
aderose <sandro.derose28@yahoo.com>
2021-01-04 15:26:32 +0000

    Test fractional lengths

--- src/tests/createShip.test.js
+++ src/tests/createShip.test.js
@@ -15,4 +15,5 @@ test('Throw error if length is not in the array [5, 4, 3, 3, 2]', () => {
   expect(() => createShip(1)).toThrow('Length can only be 2, 3, 4 or 5');
   expect(() => createShip(-100)).toThrow('Length can only be 2, 3, 4 or 5');
   expect(() => createShip(0.980394)).toThrow('Length can only be 2, 3, 4 or 5');
+  expect(() => createShip(5 / 2)).toThrow('Length can only be 2, 3, 4 or 5');
 });
