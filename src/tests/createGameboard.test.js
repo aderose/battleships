92d832a53438877a7c466faf2a8d572abba61701
aderose <sandro.derose28@yahoo.com>
2021-01-07 15:02:41 +0000

    Test incorrect place method x input

--- src/tests/createGameboard.test.js
+++ src/tests/createGameboard.test.js
@@ -25,3 +25,33 @@ test("Throw error if coordinates and length aren't passed", () => {
     'Invalid number of arguments: x, y & length are required',
   );
 });
+
+test('Throw error if x coordinate is not an integer between 0-9', () => {
+  expect(() => gameboard.place(null, 0, 2)).toThrow(
+    'x coordinate must be an integer between 0-9',
+  );
+  expect(() => gameboard.place(true, 0, 2)).toThrow(
+    'x coordinate must be an integer between 0-9',
+  );
+  expect(() => gameboard.place([], 0, 2)).toThrow(
+    'x coordinate must be an integer between 0-9',
+  );
+  expect(() => gameboard.place({}, 0, 2)).toThrow(
+    'x coordinate must be an integer between 0-9',
+  );
+  expect(() => gameboard.place('', 0, 2)).toThrow(
+    'x coordinate must be an integer between 0-9',
+  );
+  expect(() => gameboard.place(() => {}, 0, 2)).toThrow(
+    'x coordinate must be an integer between 0-9',
+  );
+  expect(() => gameboard.place(-1, 0, 2)).toThrow(
+    'x coordinate must be an integer between 0-9',
+  );
+  expect(() => gameboard.place(0.8, 0, 2)).toThrow(
+    'x coordinate must be an integer between 0-9',
+  );
+  expect(() => gameboard.place(20, 0, 2)).toThrow(
+    'x coordinate must be an integer between 0-9',
+  );
+});
