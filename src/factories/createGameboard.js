4f008b6d2774b7245dd24ef59fed0b6746a0b605
aderose <sandro.derose28@yahoo.com>
2021-01-07 14:55:27 +0000

    Throw error if required arguments are undefined

--- src/factories/createGameboard.js
+++ src/factories/createGameboard.js
@@ -4,8 +4,13 @@ const createGameboard = () => {
   );
 
   const getBoard = () => gameboard;
-
-  return { getBoard };
+  const place = (x, y, length, isHorizontal = true) => {
+    if ([x, y, length].includes(undefined))
+      throw new Error(
+        'Invalid number of arguments: x, y & length are required',
+      );
+  };
+  return { getBoard, place };
 };
 
 export default createGameboard;
