0bcead2b5cc711e7384ccda8e560012e9bd84583
aderose <sandro.derose28@yahoo.com>
2021-01-13 12:47:36 +0000

    Formatting updates

--- src/factories/createGameboard.js
+++ src/factories/createGameboard.js
@@ -5,12 +5,18 @@ const createGameboard = (size) => {
 
   const getBoard = () => gameboard;
   const place = (x, y, ship, isHorizontal = true) => {
-    if ([x, y, ship].includes(undefined))
+    if ([x, y, ship].includes(undefined)) {
       throw new Error('Arguments x, y & ship are required');
-    if (typeof x !== 'number' || x !== parseInt(x) || x < 0 || x > size - 1)
+    }
+
+    if (typeof x !== 'number' || x !== parseInt(x) || x < 0 || x > size - 1) {
       throw new Error('x coordinate must be an integer between 0-9');
-    if (typeof y !== 'number' || y !== parseInt(x) || y < 0 || y > size - 1)
+    }
+
+    if (typeof y !== 'number' || y !== parseInt(x) || y < 0 || y > size - 1) {
       throw new Error('y coordinate must be an integer between 0-9');
+    }
+
     if (
       ship === null ||
       typeof ship !== 'object' ||
@@ -23,8 +29,10 @@ const createGameboard = (size) => {
     ) {
       throw new Error('ship must be a ship object');
     }
-    if (typeof isHorizontal !== 'boolean')
+
+    if (typeof isHorizontal !== 'boolean') {
       throw new Error('isHorizontal must be a boolean');
+    }
 
     // if ship placed horizontally & it overflows the gameboard, shift it left
     // by the required number of places
