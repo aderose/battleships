40cc32fbe141760562e157d6a459043b5b5ca077
aderose <sandro.derose28@yahoo.com>
2021-01-18 16:14:05 +0000

    Ensure isRobot type is correct and correctly defaulted

--- src/factories/createPlayer.js
+++ src/factories/createPlayer.js
@@ -1,10 +1,19 @@
-const createPlayer = (player = 'Player') => {
+const createPlayer = (player = 'Player', isRobot = false) => {
   if (typeof player !== 'string') {
     throw new Error('Player name must be a string');
   }
+  if (typeof isRobot !== 'boolean') {
+    throw new Error('isRobot must be a boolean');
+  }
+
+  const makeMove = () => {};
+
+  const randomMove = () => {};
 
   return {
     getName: () => player,
+    getType: () => (isRobot ? 'Robot' : 'Human'),
+    takeTurn: isRobot ? randomMove : makeMove,
   };
 };
 
