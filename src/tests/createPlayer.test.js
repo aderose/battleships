5a6c2475dad667dece15db6d1e183f5931649d05
aderose <sandro.derose28@yahoo.com>
2021-01-18 16:13:34 +0000

    Check isRobot argument is passed and used correctly

--- src/tests/createPlayer.test.js
+++ src/tests/createPlayer.test.js
@@ -12,3 +12,20 @@ test('Player factory throws error if passed argument is not a string', () => {
 test('Passing no parameters creates the default name "Player"', () => {
   expect(createPlayer().getName()).toBe('Player');
 });
+
+test('Throw error if isRobot is not a boolean', () => {
+  expect(() => createPlayer('Player', null)).toThrow(
+    'isRobot must be a boolean',
+  );
+  expect(() => createPlayer('Player', '')).toThrow('isRobot must be a boolean');
+  expect(() => createPlayer('Player', 0)).toThrow('isRobot must be a boolean');
+  expect(() => createPlayer('Player', [])).toThrow('isRobot must be a boolean');
+  expect(() => createPlayer('Player', {})).toThrow('isRobot must be a boolean');
+  expect(() => createPlayer('Player', () => {})).toThrow(
+    'isRobot must be a boolean',
+  );
+});
+
+test('Passing no parameters creates a human', () => {
+  expect(createPlayer().getType()).toBe('Human');
+});
