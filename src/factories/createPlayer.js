ae8e8621b808226094a31a13bc8429bbeb0ce681
aderose <sandro.derose28@yahoo.com>
2021-01-19 14:52:05 +0000

    Check passed gameboard is a Gameboard object

--- src/factories/createPlayer.js
+++ src/factories/createPlayer.js
@@ -1,4 +1,34 @@
-const createPlayer = (player = 'Player', isRobot = false) => {
+const createPlayer = (gameboard, player = 'Player', isRobot = false) => {
+  if (gameboard === undefined) throw new Error('A gameboard is required');
+
+  if (Object.prototype.toString.call(gameboard).slice(8, -1) !== 'Object')
+    throw new Error('The gameboard must be an object');
+
+  // ensure gameboard object contains the listed properties
+  const {
+    getBoard,
+    getMissedShots,
+    place,
+    receiveAttack,
+    allShipsSunk,
+  } = gameboard;
+
+  // check properties are defined and are functions
+  if (
+    getBoard === undefined ||
+    getMissedShots === undefined ||
+    place === undefined ||
+    receiveAttack === undefined ||
+    allShipsSunk === undefined ||
+    typeof getBoard !== 'function' ||
+    typeof getMissedShots !== 'function' ||
+    typeof place !== 'function' ||
+    typeof receiveAttack !== 'function' ||
+    typeof allShipsSunk !== 'function'
+  ) {
+    throw new Error('gameboard must be a Gameboard object');
+  }
+
   if (typeof player !== 'string') {
     throw new Error('Player name must be a string');
   }
