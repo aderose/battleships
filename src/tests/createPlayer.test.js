ac02b94c43d53231245e6dbe6fa2be6c8bcf9dea
aderose <sandro.derose28@yahoo.com>
2021-01-19 14:51:17 +0000

    Test createPlayer with a number of non-gameboard objects

--- src/tests/createPlayer.test.js
+++ src/tests/createPlayer.test.js
@@ -1,31 +1,82 @@
 import createPlayer from '../factories/createPlayer';
+import createGameboard from '../factories/createGameboard';
 
-test('Player factory throws error if passed argument is not a string', () => {
-  expect(() => createPlayer(null)).toThrow('Player name must be a string');
-  expect(() => createPlayer(true)).toThrow('Player name must be a string');
-  expect(() => createPlayer(0)).toThrow('Player name must be a string');
-  expect(() => createPlayer([])).toThrow('Player name must be a string');
-  expect(() => createPlayer({})).toThrow('Player name must be a string');
-  expect(() => createPlayer(() => {})).toThrow('Player name must be a string');
+const gameboard = createGameboard(10);
+
+test('Passing no parameters throws an error"', () => {
+  expect(() => createPlayer()).toThrow('A gameboard is required');
+  expect(() => createPlayer(undefined)).toThrow('A gameboard is required');
 });
 
-test('Passing no parameters creates the default name "Player"', () => {
-  expect(createPlayer().getName()).toBe('Player');
+test('Throw error if passed gameboard is not an object', () => {
+  expect(() => createPlayer(null)).toThrow('The gameboard must be an object');
+  expect(() => createPlayer(true)).toThrow('The gameboard must be an object');
+  expect(() => createPlayer(0)).toThrow('The gameboard must be an object');
+  expect(() => createPlayer([])).toThrow('The gameboard must be an object');
+  expect(() => createPlayer(() => {})).toThrow(
+    'The gameboard must be an object',
+  );
+});
+
+test('Throw error if gameboard is not a Gamboard object', () => {
+  expect(() => createPlayer({})).toThrow(
+    'gameboard must be a Gameboard object',
+  );
+  expect(() => createPlayer({ prop1: '', prop2: '' })).toThrow(
+    'gameboard must be a Gameboard object',
+  );
+  expect(() => createPlayer({ prop1: () => {}, prop2: () => {} })).toThrow(
+    'gameboard must be a Gameboard object',
+  );
+  expect(() =>
+    createPlayer({
+      getBoard: '',
+      getMissedShots: '',
+      place: '',
+      receiveAttack: '',
+      allShipsSunk: '',
+    }),
+  ).toThrow('gameboard must be a Gameboard object');
+});
+
+test('Player factory throws error if passed player name is not a string', () => {
+  expect(() => createPlayer(gameboard, null)).toThrow(
+    'Player name must be a string',
+  );
+  expect(() => createPlayer(gameboard, true)).toThrow(
+    'Player name must be a string',
+  );
+  expect(() => createPlayer(gameboard, 0)).toThrow(
+    'Player name must be a string',
+  );
+  expect(() => createPlayer(gameboard, [])).toThrow(
+    'Player name must be a string',
+  );
+  expect(() => createPlayer(gameboard, {})).toThrow(
+    'Player name must be a string',
+  );
+  expect(() => createPlayer(gameboard, () => {})).toThrow(
+    'Player name must be a string',
+  );
 });
 
 test('Throw error if isRobot is not a boolean', () => {
-  expect(() => createPlayer('Player', null)).toThrow(
+  expect(() => createPlayer(gameboard, 'Player', null)).toThrow(
     'isRobot must be a boolean',
   );
-  expect(() => createPlayer('Player', '')).toThrow('isRobot must be a boolean');
-  expect(() => createPlayer('Player', 0)).toThrow('isRobot must be a boolean');
-  expect(() => createPlayer('Player', [])).toThrow('isRobot must be a boolean');
-  expect(() => createPlayer('Player', {})).toThrow('isRobot must be a boolean');
-  expect(() => createPlayer('Player', () => {})).toThrow(
+  expect(() => createPlayer(gameboard, 'Player', '')).toThrow(
     'isRobot must be a boolean',
   );
-});
-
-test('Passing no parameters creates a human', () => {
-  expect(createPlayer().getType()).toBe('Human');
+  expect(() => createPlayer(gameboard, 'Player', 0)).toThrow(
+    'isRobot must be a boolean',
+  );
+  expect(() => createPlayer(gameboard, 'Player', [])).toThrow(
+    'isRobot must be a boolean',
+  );
+  expect(() => createPlayer(gameboard, 'Player', {})).toThrow(
+    'isRobot must be a boolean',
+  );
+  expect(() => createPlayer(gameboard, 'Player', () => {})).toThrow(
+    'isRobot must be a boolean',
+  );
 });
