fb644ffb7b90f83b319675f0d6134d260d3111dd
aderose <sandro.derose28@yahoo.com>
2021-01-21 17:13:22 +0000

    Add Global styles and general page structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+import Header from './Header';
+import Game from './Game';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    background-color: #111;
+  }
+`;
 
 const App = () => {
-  return <div></div>;
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <Header />
+      <Game />
+    </React.Fragment>
+  );
 };
 
 export default App;
