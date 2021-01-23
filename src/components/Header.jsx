6c4706477f2010ec421678087a751bbda8495e3c
aderose <sandro.derose28@yahoo.com>
2021-01-23 19:13:52 +0000

    Adjust header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,47 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
 
-const Nav = styled.nav`
-  display: flex;
-  align-items: center;
-  padding: 1em 10em;
-  color: white;
-  background-color: #222;
-  margin-bottom: 2em;
-  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.3);
-`;
-
-const Title = styled.h1`
-  font-family: 'Anton';
-  letter-spacing: 5px;
-  font-size: 4em;
-  text-transform: uppercase;
-`;
-
-const Logo = styled.img`
-  filter: invert(100%) brightness(50%);
-  transform: scaleX(-1);
-  background-color: rgba(255, 255, 255, 0.25);
-  border-radius: 50%;
-  padding: 0.25em;
-  width: 150px;
-  margin-right: 2.5em;
-
-  transition: filter 0.2s ease-out;
-
-  :hover {
-    filter: invert(100%) brightness(100%);
-  }
-`;
+import { Nav, Title, Logo } from '../style';
 
 const Header = () => {
   return (
     <Nav>
+      <Title>Battleships</Title>
       <Logo
         src="https://static.thenounproject.com/png/44759-200.png"
         alt="Logo"
       />
-      <Title>Battleships</Title>
     </Nav>
   );
 };
