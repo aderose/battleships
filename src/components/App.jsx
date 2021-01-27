56c110608e70074092362233d7f6ca1747bdbcc1
aderose <35845363+aderose@users.noreply.github.com>
2021-01-27 18:21:14 +0000

    Feature/game layout (#4)
    * Add Global styles and general page structure
    
    * Import font
    
    * Add basic header styling
    
    * Adjust header styles
    
    * Add Lato font
    
    * Add a global style
    
    * Add Game container to app
    
    * Add all styled components
    
    * Create gameboard component to represent a board of cells
    
    * Add game component
    
    * Remove extra styled components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import Header from './Header';
+import Game from './Game';
+
+import { GlobalStyle } from '../style';
+
 const App = () => {
-  return <div></div>;
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <Header />
+      <Game size={10} />
+    </React.Fragment>
+  );
 };
 
 export default App;
