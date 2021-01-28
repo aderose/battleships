d67a12282db35e97985192e3377c7b303e980be7
aderose <sandro.derose28@yahoo.com>
2021-01-28 17:32:23 +0000

    Implement game loop hook to handle gameboard clicks

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -2,30 +2,48 @@ import React from 'react';
 
 import { Board, Row, Cell, Heading } from '../style';
 
+import useGameLoop from '../hooks/useGameLoop';
+
 const Gameboard = ({
   title,
   gameboard,
-  isPlaceable = true,
+  isLabelled = true,
   isInteractive = false,
+  areCellsHidden = true,
 }) => {
-  const headerRow = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+  const labels = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+
   const board = gameboard.getBoard();
+
+  const { clickHandler } = useGameLoop();
 
   return (
     <Board>
       <Heading>{title}</Heading>
-      {isPlaceable && (
+      {isLabelled && (
         <Row>
-          {headerRow.map((cell, i) => (
-            <Cell key={i}>{cell}</Cell>
+          {labels.map((label, i) => (
+            <Cell key={i} isLabel>
+              {label}
+            </Cell>
           ))}
         </Row>
       )}
-      {board.map((row, index) => (
-        <Row key={index}>
-          {isPlaceable && <Cell key={index}>{index}</Cell>}
-          {row.map((_, i) => (
-            <Cell isHoverable={isInteractive} hasBorder key={i} />
+      {board.map((row, j) => (
+        <Row key={j}>
+          {isLabelled && (
+            <Cell key={j} isLabel>
+              {j}
+            </Cell>
+          )}
+          {row.map((cell, i) => (
+            <Cell
+              key={i}
+              isActive={cell && !areCellsHidden}
+              isInteractive={isInteractive}
+              onClick={(event) => clickHandler(event, gameboard, i, j)}
+              hasBorder
+            />
           ))}
         </Row>
       ))}
