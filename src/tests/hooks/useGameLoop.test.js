f9651b5d61f7ca7728af3b72f77356dd5b97d974
aderose <sandro.derose28@yahoo.com>
2021-02-01 15:55:47 +0000

    Adjust hook tests to match refactor

--- src/tests/hooks/useGameLoop.test.js
+++ src/tests/hooks/useGameLoop.test.js
@@ -2,22 +2,11 @@ import { renderHook } from '@testing-library/react-hooks';
 
 import useGameLoop from '../../hooks/useGameLoop';
 
-test('useGameLoop contains a function called setupGame', () => {
+test('useGameLoop contains a function called startGame', () => {
   const { result } = renderHook(() => useGameLoop());
 
   expect(result.current).toMatchObject({
-    setupGame: expect.any(Function),
-  });
-});
-
-test('setupGame returns the correct object', () => {
-  const { result } = renderHook(() => useGameLoop());
-
-  expect(result.current.setupGame()).toMatchObject({
-    playerGameboard: expect.any(Object),
-    robotGameboard: expect.any(Object),
-    player: expect.any(Object),
-    robot: expect.any(Object),
+    startGame: expect.any(Function),
   });
 });
 
