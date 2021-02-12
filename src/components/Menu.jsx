8bb5f0d1f65ad174e288fa904f427715bd4c4aac
aderose <sandro.derose28@yahoo.com>
2021-02-12 17:18:29 +0000

    Call startGame callback when all ships have been placed

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,7 +6,7 @@ import createShip from '../factories/createShip';
 
 import { Container, Notification, Button } from '../style';
 
-const Menu = ({ size }) => {
+const Menu = ({ size, startGame }) => {
   const [gameboard, setGameboard] = useState(createGameboard(size));
   const [ships, setShips] = useState([5, 4, 3, 3, 2]);
   const [isHorizontal, setIsHorizontal] = useState(true);
@@ -14,11 +14,16 @@ const Menu = ({ size }) => {
 
   const swapAxis = () => setIsHorizontal(!isHorizontal);
 
+  const start = () => {
+    setIsInteractive(false);
+    setTimeout(() => startGame(gameboard), 1000);
+  };
+
   const handleClick = (x, y) => {
     gameboard.place(x, y, createShip(ships[0]), isHorizontal);
     setGameboard(gameboard);
     setShips(ships.slice(1));
-    if (ships.length === 1) setIsInteractive(false);
+    if (ships.length === 1) start();
   };
 
   return (
